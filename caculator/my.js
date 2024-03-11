d292d91b014977ca09c0cfcd9a855353fecd8905
MinhWaan <162463827+quanho119@users.noreply.github.com>
2024-03-11 21:15:29 +0700

    may tinh co ban

---
+++ caculator/my.js
@@ -0,0 +1,26 @@
+function cong()
+{
+    let a=+document.getElementById("a").value;
+    let b=+document.getElementById("b").value;
+    let x=a+b;
+    document.getElementById("nghiem").innerHTML="="+x;
+}
+function tru()
+{
+    let a=+document.getElementById("a").value;
+    let b=+document.getElementById("b").value;
+    let x=a-b;
+    document.getElementById("nghiem").innerHTML="="+x;
+}function nhan()
+{
+    let a=+document.getElementById("a").value;
+    let b=+document.getElementById("b").value;
+    let x=a*b;
+    document.getElementById("nghiem").innerHTML="="+x;
+}function chia()
+{
+    let a=+document.getElementById("a").value;
+    let b=+document.getElementById("b").value;
+    let x=a+b;
+    document.getElementById("nghiem").innerHTML="="+x;
+}
